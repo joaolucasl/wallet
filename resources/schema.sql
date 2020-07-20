bb3dc4e68b848ea9f5c60fe00bd2f96079348f4d
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2020-07-20 00:22:16 -0300

    refactor(db): pluralise Table names

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,15 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE person (
+CREATE TABLE persons (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     legalName VARCHAR,
+    displayName VARCHAR,
     motherName VARCHAR,
     birthDate DATE,
     registrationId VARCHAR(11),
-    displayName VARCHAR
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
 );
 
-CREATE TABLE address (
+CREATE TABLE addresses (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     street VARCHAR(70),
     number VARCHAR(6),
@@ -17,17 +19,19 @@ CREATE TABLE address (
     neighborhood VARCHAR,
     city VARCHAR,
     person_id uuid,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp,
     CONSTRAINT fk_person
-    FOREIGN KEY(person_id)
-    REFERENCES person(id)
-    ON DELETE CASCADE
+        FOREIGN KEY(person_id)
+        REFERENCES persons(id)
+        ON DELETE CASCADE
 );
 
-CREATE TYPE ledger_account_type AS ENUM ('Asset', 'Liability', 'Equity', 'Revenue', 'Expense');
-
-CREATE TABLE ledger_account(
+CREATE TABLE ledger_accounts (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     name VARCHAR,
     type ledger_account_type,
-    description TEXT
+    description TEXT,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
 );
