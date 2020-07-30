3408442911c557a332d3d0e62dbe4fa761c07ba0
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2020-07-29 23:03:06 -0300

    feat: Custom Serializer for CustomError

--- src/main/kotlin/com/github/joaolucasl/wallet_service/infrastructure/errors/CustomError.kt
+++ src/main/kotlin/com/github/joaolucasl/wallet_service/infrastructure/errors/CustomError.kt
@@ -1,9 +1,26 @@
 package com.github.joaolucasl.wallet_service.infrastructure.errors
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import java.lang.Exception
 
+@JsonSerialize(using = CustomErrorSerializer::class)
 data class CustomError(
     val title: String,
     val status: Int,
     val detail: String
 ) : Exception()
+
+class CustomErrorSerializer : StdSerializer<CustomError>(CustomError::class.java) {
+    override fun serialize(value: CustomError?, gen: JsonGenerator?, provider: SerializerProvider?) {
+        gen.apply {
+            this!!.writeStartObject()
+            writeStringField("title", value!!.title)
+            writeNumberField("status", value.status)
+            writeStringField("detail", value.detail)
+            writeEndObject()
+        }
+    }
+}
